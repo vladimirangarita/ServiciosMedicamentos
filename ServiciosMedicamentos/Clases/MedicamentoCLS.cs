fd8d584107237cde120dddb4c378b2ba15b261f0
vladimirangarita <vladimir.angarita@gmail.com>
2021-01-09 21:56:11 -0400

    Agregar archivos de proyecto.

---
+++ ServiciosMedicamentos/Clases/MedicamentoCLS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ServiciosMedicamentos.Clases
+{
+    [DataContract]
+    public class MedicamentoCLS
+    {
+       [DataMember(Order =0)]
+        public string Nombre { get; set; }
+        [DataMember(Order = 0)]
+        public string Concentracion { get; set; }
+        [DataMember(Order = 0)]
+        public int IidFormaFarmaceutica { get; set; }
+        [DataMember(Order = 0)]
+        public string NombreFormaFarmaceutica { get; set; }
+        [DataMember(Order = 0)]
+
+        public decimal Precio { get; set; }
+        [DataMember(Order = 0)]
+        public int Stock { get; set; }
+        [DataMember(Order = 0)]
+        public string Presentacion { get; set; }
+        public int BHabilitado { get; set; }
+
+    }
+}
