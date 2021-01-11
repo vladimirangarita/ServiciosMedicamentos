b2b8cc146b0b17ffc8d34e8d426eab4b350d8c35
vladimirangarita <vladimir.angarita@gmail.com>
2021-01-10 21:55:17 -0400

    Capitulo III ,IV listo, en desarrollo capitulo V

--- ServiciosMedicamentos/Service1.svc.cs
+++ ServiciosMedicamentos/Service1.svc.cs
@@ -5,6 +5,8 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using ServiciosMedicamentos.Models;
+using ServiciosMedicamentos.Clases;
 
 namespace ServiciosMedicamentos
 {
@@ -12,22 +14,168 @@ namespace ServiciosMedicamentos
     // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        int IService1.EliminarMedicamento(int iidMedicamento)
         {
-            return string.Format("You entered: {0}", value);
+            //throw new NotImplementedException();
+            int rpta = 0;
+
+            using (var bd=new MedicoEntities())
+            {
+
+                try
+                {
+                    Medicamento oMedicamento = bd.Medicamento.Where(P => P.IIDMEDICAMENTO == iidMedicamento).First();
+                    oMedicamento.BHABILITADO = 0;
+                    bd.SaveChanges();
+                    rpta = 1;
+                }
+                catch (Exception)
+                {
+                    rpta = 0;
+                    //throw;
+                }
+
+
+                return rpta;
+            }
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        List<FormaFarmaceuticaCLS> IService1.ListaFormaFarmaceutica()
         {
-            if (composite == null)
+            //throw new NotImplementedException();
+            List<FormaFarmaceuticaCLS> ListaFormaFarmaceutica =
+                new List<FormaFarmaceuticaCLS>();
+
+            try
             {
-                throw new ArgumentNullException("composite");
+                using (var bd = new MedicoEntities())
+                {
+                    ListaFormaFarmaceutica = bd.FormaFarmaceutica.Where(p=>p.BHABILITADO==1)
+                        .Select(p => new FormaFarmaceuticaCLS
+                        {
+                            IidFormaFarmaceutica = p.IIDFORMAFARMACEUTICA,
+                            NombreFormaFarmaceutica = p.NOMBRE
+                        }).ToList();
+                }
             }
-            if (composite.BoolValue)
+            catch (Exception)
             {
-                composite.StringValue += "Suffix";
+
+                ListaFormaFarmaceutica = null;
             }
-            return composite;
+            return ListaFormaFarmaceutica;
+        }
+
+        List<MedicamentoCLS> IService1.ListarMedicamentos()
+        {
+            //throw new NotImplementedException();
+            List<MedicamentoCLS> ListaMedicamento = new List<MedicamentoCLS>;
+            try
+            {
+                using (var bd = new MedicoEntities())
+                {
+                    ListaMedicamento = (from medicamento in bd.Medicamento
+                                        join formafarmaceutica in bd.FormaFarmaceutica
+                                        on medicamento.IIDFORMAFARMACEUTICA equals
+                                        formafarmaceutica.IIDFORMAFARMACEUTICA
+                                        select new MedicamentoCLS
+                                        {
+                                            IidMedicamento = medicamento.IIDMEDICAMENTO,
+                                            Nombre = medicamento.NOMBRE,
+                                            Precio = (decimal)medicamento.PRECIO,
+                                            NombreFormaFarmaceutica = formafarmaceutica.NOMBRE,
+                                            Concentracion = medicamento.CONCENTRACION,
+                                            Presentacion = medicamento.PRESENTACION,
+                                            Stock = (int)medicamento.STOCK,
+                                            BHabilitado=(int)medicamento.BHABILITADO
+                                        }).ToList();
+                }
+            }
+            catch (Exception)
+            {
+
+                ListaMedicamento = null;
+            }
+
+        }
+
+        MedicamentoCLS IService1.RecuperarMedicamento(int iidMedicamento)
+        {
+            //throw new NotImplementedException();
+            MedicamentoCLS oMedicamentoCLS = new MedicamentoCLS();
+            try
+            {
+                using (var bd = new MedicoEntities())
+                {
+                    Medicamento oMedicamento = bd.Medicamento.Where(p => p.IIDMEDICAMENTO == iidMedicamento).First();
+                    oMedicamentoCLS.IidMedicamento = oMedicamento.IIDMEDICAMENTO;
+                    oMedicamentoCLS.IidFormaFarmaceutica =(int) oMedicamento.IIDFORMAFARMACEUTICA;
+                    oMedicamentoCLS.Nombre = oMedicamento.NOMBRE;
+                    oMedicamentoCLS.Precio =(decimal) oMedicamento.PRECIO;
+                    oMedicamentoCLS.Stock = (int)oMedicamento.STOCK;
+                    oMedicamentoCLS.Concentracion = oMedicamento.CONCENTRACION;
+                    oMedicamentoCLS.Presentacion = oMedicamento.PRESENTACION;
+
+                }
+            }
+            catch (Exception)
+            {
+
+                oMedicamentoCLS = null;
+            }
+            return oMedicamentoCLS;
+        }
+
+        int IService1.RegistraryActualizarMedicamento(MedicamentoCLS oMedicamentoCLS)
+        {
+            //throw new NotImplementedException();
+            int rpta = 0;
+            try
+            {
+                using (var bd=new MedicoEntities())
+                {
+                    if (oMedicamentoCLS.IidFormaFarmaceutica==0)
+                    {
+                        Medicamento oMedicamento = new Medicamento();
+                        oMedicamento.IIDMEDICAMENTO = oMedicamentoCLS.IidMedicamento;
+
+                        oMedicamento.NOMBRE = oMedicamentoCLS.Nombre;
+                        oMedicamento.PRECIO = oMedicamentoCLS.Precio;
+                        oMedicamento.STOCK = oMedicamentoCLS.Stock;
+                        oMedicamento.IIDFORMAFARMACEUTICA = oMedicamentoCLS.IidFormaFarmaceutica;
+                        oMedicamento.CONCENTRACION = oMedicamentoCLS.Concentracion;
+                        oMedicamento.PRESENTACION = oMedicamentoCLS.Presentacion;
+                        oMedicamento.BHABILITADO = 1;
+
+                        bd.Medicamento.Add(oMedicamento);
+                        bd.SaveChanges();
+                        rpta = 1;
+                    }
+                    else
+                    {
+                        Medicamento oMedicamento = bd.Medicamento.Where(p => p.IIDMEDICAMENTO == oMedicamentoCLS.IidMedicamento).First();
+
+
+                        oMedicamento.NOMBRE = oMedicamentoCLS.Nombre;
+                        oMedicamento.PRECIO = oMedicamentoCLS.Precio;
+                        oMedicamento.STOCK = oMedicamentoCLS.Stock;
+                        oMedicamento.IIDFORMAFARMACEUTICA = oMedicamentoCLS.IidFormaFarmaceutica;
+                        oMedicamento.CONCENTRACION = oMedicamentoCLS.Concentracion;
+                        oMedicamento.PRESENTACION = oMedicamentoCLS.Presentacion;
+                        bd.SaveChanges();
+                        rpta = 1;
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                rpta = 0;
+
+            }
+
+            return rpta;
         }
     }
 }
